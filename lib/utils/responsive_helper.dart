ba468c4616b13eb7b96bed1bdc0e35bd394aa370
Shaimaa-22 <shaimaa.dwedar@students.alquds.edu>
2025-08-28 15:53:55 +0300

    Update responsive_helper.dart
    عملت انه يكون ال RESPONSIVE بين ال 1.2 و ال 0.7 و ما اعطيت قيم محدده عشان اسهل ع حالي انه  يكون التحكم بالحجم احسن مع اختلاف حجم الاجهزه ببساطه هيك فليكسبل اكتر يعني

--- lib/utils/responsive_helper.dart
+++ lib/utils/responsive_helper.dart
@@ -1,67 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ResponsiveHelper {
-  static double screenWidth(BuildContext context) {
-    return MediaQuery.of(context).size.width;
+  static double screenWidth(BuildContext context) =>
+      MediaQuery.of(context).size.width;
+
+  static double screenHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
+
+  static bool isMobile(BuildContext context) => screenWidth(context) < 600;
+
+  static bool isTablet(BuildContext context) =>
+      screenWidth(context) >= 600 && screenWidth(context) < 1024;
+
+  static bool isDesktop(BuildContext context) => screenWidth(context) >= 1024;
+
+  static double responsiveValue(
+    BuildContext context, {
+    required double base,
+    double minScale = 0.7,
+    double maxScale = 1.2,
+  }) {
+    if (isMobile(context)) {
+      return base * minScale;
+    } else if (isTablet(context)) {
+      return base;
+    } else {
+      return base * maxScale;
+    }
   }
 
-  static double screenHeight(BuildContext context) {
-    return MediaQuery.of(context).size.height;
-  }
-
-  static bool isMobile(BuildContext context) {
-    return screenWidth(context) < 600;
-  }
-
-  static bool isTablet(BuildContext context) {
-    return screenWidth(context) >= 600 && screenWidth(context) < 1024;
-  }
-
-  static bool isDesktop(BuildContext context) {
-    return screenWidth(context) >= 1024;
-  }
-
-  // Responsive padding
   static EdgeInsets responsivePadding(BuildContext context) {
-    if (isMobile(context)) {
-      return const EdgeInsets.all(16);
-    } else if (isTablet(context)) {
-      return const EdgeInsets.all(24);
-    } else {
-      return const EdgeInsets.all(32);
-    }
+    final value = responsiveValue(context, base: 24);
+    return EdgeInsets.all(value);
   }
 
-  // Responsive margin
   static EdgeInsets responsiveMargin(BuildContext context) {
-    if (isMobile(context)) {
-      return const EdgeInsets.all(8);
-    } else if (isTablet(context)) {
-      return const EdgeInsets.all(12);
-    } else {
-      return const EdgeInsets.all(16);
-    }
+    final value = responsiveValue(context, base: 12);
+    return EdgeInsets.all(value);
   }
 
-  static double titleFontSize(BuildContext context) {
-    if (isMobile(context)) {
-      return 20;
-    } else if (isTablet(context)) {
-      return 24;
-    } else {
-      return 28;
-    }
-  }
+  static double titleFontSize(BuildContext context) =>
+      responsiveValue(context, base: 22);
 
-  static double bodyFontSize(BuildContext context) {
-    if (isMobile(context)) {
-      return 14;
-    } else if (isTablet(context)) {
-      return 16;
-    } else {
-      return 18;
-    }
-  }
+  static double bodyFontSize(BuildContext context) =>
+      responsiveValue(context, base: 16);
 
   static double containerWidth(BuildContext context) {
     final width = screenWidth(context);
@@ -70,27 +52,16 @@ class ResponsiveHelper {
     } else if (isTablet(context)) {
       return width * 0.7;
     } else {
-      return 600;
+      return responsiveValue(context, base: 500);
     }
   }
 
-  static double animationSize(BuildContext context) {
-    if (isMobile(context)) {
-      return screenWidth(context) * 0.6;
-    } else if (isTablet(context)) {
-      return 350;
-    } else {
-      return 400;
-    }
-  }
+  static double animationSize(BuildContext context) =>
+      responsiveValue(context, base: 300);
 
   static int gridColumns(BuildContext context) {
-    if (isMobile(context)) {
-      return 2;
-    } else if (isTablet(context)) {
-      return 3;
-    } else {
-      return 4;
-    }
+    if (isMobile(context)) return 2;
+    if (isTablet(context)) return 3;
+    return 4;
   }
 }
